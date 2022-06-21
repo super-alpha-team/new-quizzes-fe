daf86568bcd0efe7a166783b0c1254e9ee3fe0c8
nxmai <nxmai.it@gmail.com>
2022-06-21 16:44:00 +0700

    fix joining card color

--- components/launch/JoiningCard.jsx
+++ components/launch/JoiningCard.jsx
@@ -10,7 +10,7 @@ function JoiningCard({ name, ava, ...rest }) {
             className="w-64 rounded flex justify-center flex-col items-center overflow-hidden shadow-lg border-[1px] border-gray-100 pb-4 hover:transform hover:scale-110 hover:duration-200 text-center"
         >
             <div className="w-40 h-40"><BigHead {...ava} /></div>
-            <p className='text-xl p-2 font-bold break-words' style={{ color: randomHexColor() }}>{name}</p>
+            <p className='text-xl p-2 font-bold break-words'>{name}</p>
         </div>
     );
 }
