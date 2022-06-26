f2009734ff8b150638f73e87d421e58ac2694b2a
nxmai <nxmai.it@gmail.com>
2022-06-26 17:30:26 +0700

    add b tag

--- components/articles/HomePage.jsx
+++ components/articles/HomePage.jsx
@@ -20,26 +20,26 @@ function HomePage() {
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 2: Creating a new credential</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
                     <li>Login (or create a new account) in <a href={`/platform`} className='hover:text-qpurple text-qpurple-light underline italic' target="blank" rel="noopener noreferrer">LTI Configuration Page</a></li>
-                    <li>Click Create a new LTI credential.</li>
-                    <li>Enter new credential name and click Next.</li>
+                    <li>Click <b> Create a new LTI credential </b>.</li>
+                    <li>Enter <b> new credential name </b> and click <b> Next </b> .</li>
                     <li>Back to Moodle and follow the instructions below. (Keep this page open for further configurations)</li>
                 </ol>
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 3: Creating a Client ID in Moodle</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
                     <li>Log in to Moodle as an Admin.</li>
                     <li>Click <b> Site administration </b>, then <b> Plugins </b>.</li>
-                    <li>Click External tool, then Manage tools.</li>
+                    <li>Click <b> External tool </b>, then Manage tools.</li>
                     <li>Click configure a tool manually.</li>
                 </ol>
-                <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 4: Configuring the external tool</h3>
+                <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 4: Configuring the External tool</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
-                    <li>Enter "NewQuizzes" in the Tool name field.</li>
-                    <li>Copy the Target Link URL in the LTI Configuration Page, and paste it in the Tool URL field.</li>
+                    <li>Enter "<b> NewQuizzes </b>" in the Tool name field.</li>
+                    <li>Copy the <b> Target Link URL </b> in the LTI Configuration Page, and paste it in the Tool URL field.</li>
                     <li>For LTI version, choose LTI 1.3.</li>
                     <li>For Public key type, choose Keyset URL.</li>
-                    <li>Copy the Public JWK URL in the LTI Configuration Page, and paste it in the Public keyset field.</li>
-                    <li>Copy the Tool Redirect URL in the LTI Configuration Page, and paste it in the Redirection URI(s) field.</li>
-                    <li>Copy the Login Initiation URL in the LTI Configuration Page to, and paste it in the Initiate login URL field.</li>
+                    <li>Copy the <b> Public JWK URL </b> in the LTI Configuration Page, and paste it in the Public keyset field.</li>
+                    <li>Copy the <b> Tool Redirect URL </b> in the LTI Configuration Page, and paste it in the Redirection URI(s) field.</li>
+                    <li>Copy the <b> Login Initiation URL </b> in the LTI Configuration Page to, and paste it in the Initiate login URL field.</li>
                     <li>At Tool configuration usage: Choose "Show in activity chooser and as a preconfigured tool"</li>
                     <li>At Default launch container: Choose "New window"</li>
                     <li>At Services Section:
@@ -52,7 +52,7 @@ function HomePage() {
                         <br />{`Share launcher's email with tool: "Always"`}
                         <br />{`Accept grades from the tool: "Always"`}
                     </li>
-                    <li>Click Save Changes.</li>
+                    <li>Click <b> Save Changes</b>.</li>
                 </ol>
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 5: Continuing configure </h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
@@ -64,19 +64,20 @@ function HomePage() {
                     </li>
                     <li>Back to LTI Configuration Page, Copy Platform ID and Client ID and parse them in corresponding fields.</li>
                     <li>With Access Token field, back to Moodle.</li>
-                    <li>Click <b> Site administration </b>, then Server.</li>
-                    <li>Click Manage tokens at Web services section.</li>
-                    <li>Copy the Token of Admin User and parse it in the Access Token field of LTI Configuration Page.</li>
-                    <li>Click Save. Now your LTI plugin is ready!</li>
+                    <li>Click <b> Site administration </b>, then <b> Server</b>.</li>
+                    <li>Click <b> Manage tokens </b> at Web services section.</li>
+                    <li>Copy the Token of Admin User if you already have and parse it in the Access Token field of LTI Configuration Page. 
+                        If not, create one 
+                    </li>
+                    <li>Click <b> Save </b>. Now your LTI plugin is ready!</li>
                 </ol>
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 5: Adding the Tool to Moodle Course</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
                     <li>On Moodle go to the course that you want to add the Tool to.</li>
                     <li>Click Turn editing on, then click Add an activity or resource.</li>
-                    <li>Click External tool.</li>
+                    <li>Click <b> NewQuizzes </b> (tool that you created).</li>
                     <li>For Activity name, enter the name of the activity.</li>
-                    <li>For Preconfigured tool, choose the Tool created earlier.</li>
-                    <li>Click Save and return to course.</li>
+                    <li>Click <b> Save </b> and return to course.</li>
                 </ol>
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Step 6: Launching the LTI 1.3 tool</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
