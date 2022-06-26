586047af4441cc7f449560c555425b7d955238d0
Kim Ngan Dinh Phan <kimngandinhphan@gmail.com>
2022-06-26 16:23:59 +0700

    add instructions

--- components/articles/HomePage.jsx
+++ components/articles/HomePage.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import { CLIENT_URL, SERVER_URL } from 'utils/config';
 
 function HomePage() {
     return (
@@ -13,14 +14,15 @@ function HomePage() {
                     <li>Log in to Moodle as an Admin.</li>
                     <li>Click Site administration, then Plugins.</li>
                     <li>Click Install plugins.</li>
-                    <li>Click <a className='hover:text-qpurple text-qpurple-light underline' href='https://drive.google.com/file/d/1EmUvGNBcLfkFyDOCL5KZ52_Jrmua09yp/view?usp=sharing' target="blank">here</a> to download .zip file and upload to Moodle.</li>
+                    <li>Click <a className='hover:text-qpurple text-qpurple-light underline italic' href='https://drive.google.com/file/d/1EmUvGNBcLfkFyDOCL5KZ52_Jrmua09yp/view?usp=sharing' target="blank">here</a> to download .zip file and upload to Moodle.</li>
                     <li>Submit.</li>
                 </ol>
-                <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Creating new credentials</h3>
+                <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Creating a new credential</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
-                    <li>Create a new LTI credential.</li>
-                    <li>(Optional) Update a credential from LTI 1.1. to LTI 1.3. in the configuration page by choosing LTI 1.3 under Select which LTI version to use.</li>
-                    <li>Click Save.</li>
+                    <li>Login (or create a new account) in <a href={`/platform`} className='hover:text-qpurple text-qpurple-light underline italic' target="blank" rel="noopener noreferrer">LTI Configuration Page</a></li>
+                    <li>Click Create a new LTI credential.</li>
+                    <li>Enter new credential name and click Next.</li>
+                    <li>Back to Moodle and follow the instructions below. (Keep this page open for further configurations)</li>
                 </ol>
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Creating a Client ID in Moodle</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
@@ -32,12 +34,12 @@ function HomePage() {
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Configuring the external tool</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
                     <li>Enter "NewQuizzes" in the Tool name field.</li>
-                    <li>Copy the Target Link URL in the LTI configuration page, and paste it in the Tool URL field.</li>
+                    <li>Copy the Target Link URL in the LTI Configuration Page, and paste it in the Tool URL field.</li>
                     <li>For LTI version, choose LTI 1.3.</li>
                     <li>For Public key type, choose Keyset URL.</li>
-                    <li>Copy the Public JWK URL in the LTI configuration page, and paste it in the Public keyset field.</li>
-                    <li>Copy the Tool Redirect URL in the LTI configuration page, and paste it in the Redirection URI(s) field.</li>
-                    <li>Copy the Login Initiation URL in the LTI configuration page to, and paste it in the Initiate login URL field.</li>
+                    <li>Copy the Public JWK URL in the LTI Configuration Page, and paste it in the Public keyset field.</li>
+                    <li>Copy the Tool Redirect URL in the LTI Configuration Page, and paste it in the Redirection URI(s) field.</li>
+                    <li>Copy the Login Initiation URL in the LTI Configuration Page to, and paste it in the Initiate login URL field.</li>
                     <li>At Tool configuration usage: Choose "Show in activity chooser and as a preconfigured tool"</li>
                     <li>At Default launch container: Choose "New window"</li>
                     <li>At Services Section:
@@ -52,15 +54,16 @@ function HomePage() {
                     </li>
                     <li>Click Save Changes.</li>
                 </ol>
-                <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Adding the Client ID to LTI Pro Credentials</h3>
+                <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Continuing </h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
                     <li>Find the Tool that was created earlier in the Manage tools page of Moodle.</li>
                     <li>Click View configuration details.</li>
-                    <li>On LTI Pro Configuration page, click 3rd Party Credentials.</li>
-                    <li>Click LTI Moodle, then click Add Instance.</li>
-                    <li>Add the domain used to access Moodle in the LTI Moodle Site Domain.</li>
-                    <li>Copy the Moodle tool Client ID, and paste it into the LTI Advantage Client ID field.</li>
-                    <li>Click Save.</li>
+                    <li>Back to LTI Configuration Page, Copy Platform ID and Client ID and parse them in corresponding fields.</li>
+                    <li>With Access Token field, back to Moodle.</li>
+                    <li>Click Site administration, then Server.</li>
+                    <li>Click Manage tokens at Web services section.</li>
+                    <li>Copy the Token of Admin User and parse it in the Access Token field of LTI Configuration Page.</li>
+                    <li>Click Save. Now your LTI plugin is ready!</li>
                 </ol>
                 <h3 className='text-2xl mt-4 mb-1 text-qpurple font-bold'>Adding the Tool to Moodle Course</h3>
                 <ol className='list-decimal pl-20 py-1 leading-7'>
